863d733ec44384afe87b18bcac0a3e15b7d75bbc
Crowdbotics <team@crowdbotics.com>
2024-02-28 16:51:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "conscious-ai-compan-46695",
+  url: "https://conscious-ai-compan-46695.botics.co",
+  api: "https://conscious-ai-compan-46695.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
